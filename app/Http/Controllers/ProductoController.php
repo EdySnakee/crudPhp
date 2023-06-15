2fe5d97184220f526553a7ef7f3efb5b735e7193
Edy <contactoedysnake@gmail.com>
2023-06-15 15:31:54 -0600

    estructura base

---
+++ app/Http/Controllers/ProductoController.php
@@ -0,0 +1,54 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Producto;
+use Carbon\Carbon;
+
+class ProductoController extends Controller{
+
+    public function index (){
+
+        $datosProducto= Producto::all(); 
+
+        return response()->json($datosProducto);
+    }
+
+public function guardar (Request $request){
+
+
+    $datosProducto= new Producto;
+    
+
+if($request->hasfile('imagen')){
+
+    $nombreArchivoOriginal=$request->file('imagen')->getClientOriginalName();
+    $nuevoNombre=Carbon::now()->timestamp."_".$nombreArchivoOriginal;
+    $carpetaDestino='./upload/';
+    $request->file('imagen')->move($carpetaDestino, $nuevoNombre);
+
+    $datosProducto->titulo=$request->titulo;
+    $datosProducto->imagen=ltrim($carpetaDestino, '.').$nuevoNombre;
+    $datosProducto->save();
+}
+return response()->json($nuevoNombre);
+ }
+
+public function ver($id){
+
+    $datosProducto= new Producto;
+    $datosEncontrados=$datosProducto->find($id);
+    return response()->json($datosEncontrados);
+}
+
+
+public function eliminar($id) {
+
+    return response("Borrar");
+
+    
+}
+
+
+
+}
